b571d26f06c2f8fa579a9ed5b1fac1e7383f9a96
Your Name <20373370@buaa.edu.cn>
2022-07-22 14:39:41 +0800

    refresh

--- UE4_project/Pretest1/Source/Pretest1/Pretest1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Pretest1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pretest1, "Pretest1" );
- 
